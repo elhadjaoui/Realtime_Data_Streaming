95ebf8a2534f54f32debb33f5b6d8bbc2078988e
elhadjaoui <achraf-2060@outlook.fr>
2024-10-31 15:31:59 +0100

    Add schema for JSON parsing in spark-streaming.py

--- src/jobs/spark-streaming.py
+++ src/jobs/spark-streaming.py
@@ -1,10 +1,22 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, when, udf
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
+
 
 def streaming(spark):
     df = (spark.readStream.format("socket")
     .option("host", "localhost") 
     .option("port", 9999).load())
     
+    schema = StructType([
+                StructField("review_id", StringType()),
+                StructField("user_id", StringType()),
+                StructField("business_id", StringType()),
+                StructField("stars", FloatType()),
+                StructField("date", StringType()),
+                StructField("text", StringType())
+            ])
+    df = df.select(from_json(col("value"), schema).alias("data")).select("data.*")
     query = df.writeStream.outputMode('append').format("console").start()
     query.awaitTermination()
 
