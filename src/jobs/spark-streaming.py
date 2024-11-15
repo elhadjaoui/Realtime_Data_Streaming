945d70a622aa7fa720ca495ae4c5853439f89bd4
elhadjaoui <achraf-2060@outlook.fr>
2024-11-15 01:48:16 +0100

    Add Kafka integration and customer review schema

--- src/jobs/spark-streaming.py
+++ src/jobs/spark-streaming.py
@@ -1,14 +1,45 @@
+from time import sleep
+import openai
+import sys
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, when, udf
 from pyspark.sql.types import StructType, StructField, StringType, FloatType
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from config.config import config
 
+def sentiment_analysis(comment) -> str:
+    if comment:
+        openai.api_key = config['openai']['api_key']
+        completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages = [
+                {
+                    "role": "system",
+                    "content": """
+                        You're a machine learning model with a task of classifying comments into POSITIVE, NEGATIVE, NEUTRAL.
+                        You are to respond with one word from the option specified above, do not add anything else.
+                        Here is the comment:
+                        
+                        {comment}
+                    """.format(comment=comment)
+                }
+            ]
+        )
+        return completion.choices[0].message['content']
+    return "Empty"
 
-def streaming(spark):
-    df = (spark.readStream.format("socket")
-    .option("host", "localhost") 
-    .option("port", 9999).load())
-    
-    schema = StructType([
+def start_streaming(spark):
+    topic = 'customers_review'
+    while True:
+        try:
+            stream_df = (spark.readStream.format("socket")
+                         .option("host", "localhost")
+                         .option("port", 9999)
+                         .load()
+                         )
+
+            schema = StructType([
                 StructField("review_id", StringType()),
                 StructField("user_id", StringType()),
                 StructField("business_id", StringType()),
@@ -16,16 +47,37 @@ def streaming(spark):
                 StructField("date", StringType()),
                 StructField("text", StringType())
             ])
-    df = df.select(from_json(col("value"), schema).alias("data")).select("data.*")
-    query = df.writeStream.outputMode('append').format("console").start()
-    query.awaitTermination()
+
+            stream_df = stream_df.select(from_json(col('value'), schema).alias("data")).select(("data.*"))
+
+            # sentiment_analysis_udf = udf(sentiment_analysis, StringType())
+
+            # stream_df = stream_df.withColumn('feedback', when(col('text').isNotNull(), sentiment_analysis_udf(col('text'))) .otherwise(None))
+
+            kafka_df = stream_df.selectExpr("CAST(review_id AS STRING) AS key", "to_json(struct(*)) AS value")
+
+            query = (kafka_df.writeStream
+                   .format("kafka")
+                   .option("kafka.bootstrap.servers", config['kafka']['bootstrap.servers'])
+                   .option("kafka.security.protocol", config['kafka']['security.protocol'])
+                   .option('kafka.sasl.mechanism', config['kafka']['sasl.mechanisms'])
+                   .option('kafka.sasl.jaas.config',
+                           'org.apache.kafka.common.security.plain.PlainLoginModule required username="{username}" '
+                           'password="{password}";'.format(
+                               username=config['kafka']['sasl.username'],
+                               password=config['kafka']['sasl.password']
+                           ))
+                   .option('checkpointLocation', '/tmp/checkpoint')
+                   .option('topic', topic)
+                   .start()
+                   .awaitTermination()
+                )
+
+        except Exception as e:
+            print(f'Exception encountered: {e}. Retrying in 10 seconds')
+            sleep(10)
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("SparkStreaming").getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
-    streaming(spark)
-    
+    spark_conn = SparkSession.builder.appName("SocketStreamConsumer").getOrCreate()
 
-    
-    
-    spark.stop()
+    start_streaming(spark_conn)
